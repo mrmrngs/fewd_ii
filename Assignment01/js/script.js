54da2912be812c87eab3bebebdfbdc9a4b01d102
mrmrngs <84865841+mrmrngs@users.noreply.github.com>
2022-02-23 20:42:16 -0500

    Assignment 1 Push

---
+++ Assignment01/js/script.js
@@ -0,0 +1,124 @@
+// STEP 1
+/* let someMonth;
+function theMonth();
+let currentMonth;
+let summerMonth;
+let myLibraryFunction; */
+
+// STEP 2
+/* 3;
+'Hello!';
+true;
+null; */
+
+// STEP 3
+/* let varEx = 3 * (4 + 5);
+let compEx = varEx + ' ' + undefEx; */
+
+// STEP 4
+/* let firstName;
+let lastName;
+let strAddress;
+let strCity;
+let strState;
+let strZip;
+let strYourAge;
+let referralSource;
+let mayWeContactYou; */
+
+// STEP 5
+/* let firstName = 'Miriam'
+var lastName = 'Neigus'
+const mayWeContactYou = 'May we contact you?'; */
+
+// STEP 6
+/* let strAddress = 13964 + ' ' + 'Recuerdo Drive';
+console.log(strAddress); */
+
+// STEP 7
+/* let var1 = (1 > 0) + ' is true.'
+console.log(var1);
+let var2 = 92014 + ('string' == 'string');
+console.log(var2); */
+
+// STEP 8
+/* The string literal is not valid. It needs a backslash \ in front of the apostrophe in "I'm". */
+
+// STEP 9
+/* let nullVar = null;
+console.log(nullVar);
+let undefVar;
+console.log(undefVar); */
+
+// STEP 10
+/* let newStr = 'Hello!';
+console.log(typeof newStr);
+let typeNum = 5;
+console.log(typeof typeNum);
+let typeBool = 5 > 4;
+console.log(typeof typeBool);
+let typeObj = new Object();
+console.log(typeof typeObj);
+let typeUndef;
+console.log(typeof typeUndef); */
+
+// STEP 11
+/* alert('Hello ' + 'Miriam Neigus' + ', welcome to the JavaScript class!'); */
+
+// STEP 12
+/* let name = 'Miriam Neigus';
+alert('Hello ' + name + ', welcome to the JavaScript class!'); */
+
+// STEP 13
+/* let name = 'Miriam Neigus';
+let course = 'Javascript';
+alert('Hello ' + name + ', welcome to the ' + course + ' class!'); */
+
+// STEP 14
+/* let name = 'Miriam Neigus';
+let course = 'Javascript';
+alert('Hello ' + name + '.\nWelcome to the ' + course + ' class!'); */
+
+// STEP 15
+/* let userName = prompt('What is your name?')
+let course = 'Javascript';
+alert('Hello ' + userName + '.\nWelcome to the ' + course + ' class!'); */
+
+// STEP 16
+/* let userName = prompt('What is your name?')
+let userCourse = prompt('What is the title of the course you are taking?');
+alert('Hello ' + userName + '.\nWelcome to the ' + userCourse + ' class!'); */
+
+// STEP 17
+/* let x = 10;
+let y = 20;
+console.log(x + y); */
+
+// STEP 18
+/* let x = 20;
+x += 20;
+console.log(x); */
+
+// STEP 19
+/* let x = 20;
+x *= 5;
+console.log(x); */
+
+// STEP 20
+/* let x = 20 % 3;
+x /= 1;
+console.log(x); */
+
+// STEP 21
+/* let x = 12;
+let y = 24;
+let z = (x < y) || (y > 0);
+console.log(z); */
+
+// STEP 22
+/* let x = 12;
+let y = 24;
+let z = (x >= y) && (y === '');
+console.log(z); */
+
+
